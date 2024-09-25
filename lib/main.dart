a65a9aa2c51c2b4236154679a2da090d01375a05
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-09-25 23:53:14 +0300

    added permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/bloc/weather_bloc_bloc.dart';
 import 'package:weather_app/screens/home_screen.dart';
 
 void main() {
@@ -8,11 +11,53 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  Future<Position> fetchData() async {
+    // Request permission for location
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      throw Exception("Location permission denied");
+    }
+
+    // Get the user's current position
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: FutureBuilder<Position>(
+        future: fetchData(),
+        builder: (context, snap) {
+          if (snap.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else if (snap.hasError) {
+            return const Scaffold(
+              body: Center(
+                child: Text('Error fetching location data'),
+              ),
+            );
+          } else if (snap.hasData) {
+            return BlocProvider<WeatherBlocBloc>(
+              create: (context) => WeatherBlocBloc()
+                ..add(FetchWeather(
+                    snap.data!)), // Use snap.data! to assert it's not null
+              child: HomeScreen(),
+            );
+          } else {
+            return const Scaffold(
+              body: Center(
+                child: Text('No location data available'),
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
